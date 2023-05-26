78ac2575c6c3ce3b732c0ab97708513fa926f79a
haen-su <haensuya@gmail.com>
2023-05-26 15:45:58 +0900

    feat: 맞춤 추천작품 조회 api 추가

--- src/main/java/com/oven/server/api/work/controller/HomeController.java
+++ src/main/java/com/oven/server/api/work/controller/HomeController.java
@@ -5,6 +5,7 @@ import com.oven.server.api.response.Response;
 import com.oven.server.api.response.ResponseStatus;
 import com.oven.server.api.work.dto.response.GetWorkDto;
 import com.oven.server.api.work.service.GetPopularWorkListService;
+import com.oven.server.api.work.service.GetRecommendWorksService;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -18,6 +19,7 @@ import java.util.List;
 public class HomeController {
 
     private final GetPopularWorkListService getPopularWorkListService;
+    private final GetRecommendWorksService getRecommendWorkListService;
 
     @GetMapping("/home/populars")
     public Response<List<GetWorkDto>> getPopularWorkList() {
@@ -28,6 +30,17 @@ public class HomeController {
             return new Response<>(e.getResponseStatus());
         }
     }
+
+    @GetMapping("/home/recommendation/works")
+    public Response<List<GetWorkDto>> getRecommendWorkList() {
+        try {
+            List<GetWorkDto> recommendWorkDtoList = getRecommendWorkListService.getRecommendWorkList();
+            return new Response<>(recommendWorkDtoList, ResponseStatus.SUCCESS);
+        } catch (BaseException e) {
+            return new Response<>(e.getResponseStatus());
+        }
+    }
+
 
 
 
--- src/main/java/com/oven/server/api/work/service/GetRecommendWorksService.java
+++ src/main/java/com/oven/server/api/work/service/GetRecommendWorksService.java
@@ -11,25 +11,23 @@ import java.util.stream.Collectors;
 
 @Service
 @RequiredArgsConstructor
-public class GetPopularWorkListService {
+public class GetRecommendWorksService {
 
-    public final WorkRepository workRepository;
+    private final WorkRepository workRepository;
 
-    public List<GetWorkDto> getPopularWorkList() throws BaseException {
+    public List<GetWorkDto> getRecommendWorkList() throws BaseException {
 
-        // 일단 크롤링 한 대로 추천 순위 상위 10개 가져옴
-        List<GetWorkDto> popularWorkDtoList = workRepository.findTop10ByOrderByIdAsc()
+        List<GetWorkDto> recommendWorkDtoList = workRepository.findTop10ByOrderByIdAsc()
                 .stream()
                 .map(
-                        popularWork -> GetWorkDto.builder()
-                                .title(popularWork.getTitleKr())
-                                .poster(popularWork.getPoster())
+                        recommendWork -> GetWorkDto
+                                .builder()
+                                .title(recommendWork.getTitleKr())
+                                .poster(recommendWork.getPoster())
                                 .build()
                 )
                 .collect(Collectors.toList());
 
-        return popularWorkDtoList;
-
+        return recommendWorkDtoList;
     }
-
 }
