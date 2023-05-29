a3bbd69d047f735a11a704d999c04a164b5e074f
haen-su <haensuya@gmail.com>
2023-05-29 15:35:16 +0900

    feat: 작품 좋아요 api 추가

---
+++ src/main/java/com/oven/server/api/user/service/InterestingWorkService.java
@@ -0,0 +1,44 @@
+package com.oven.server.api.user.service;
+
+import com.oven.server.api.response.BaseException;
+import com.oven.server.api.response.ResponseStatus;
+import com.oven.server.api.user.domain.InterestingWork;
+import com.oven.server.api.user.domain.User;
+import com.oven.server.api.user.repository.InterestingWorkRepository;
+import com.oven.server.api.user.repository.UserRepository;
+import com.oven.server.api.work.domain.Work;
+import com.oven.server.api.work.repository.WorkRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@Service
+@Transactional
+@RequiredArgsConstructor
+public class InterestingWorkService {
+
+    private final InterestingWorkRepository interestingWorkRepository;
+    private final UserRepository userRepository;
+    private final WorkRepository workRepository;
+
+    public void postInterestingWork(@AuthenticationPrincipal User user, Long workId) throws BaseException {
+
+        userValidate(user);
+        Work work = workValidate(workId);
+
+        InterestingWork newInterestingWork = new InterestingWork(user, work);
+        interestingWorkRepository.save(newInterestingWork);
+
+    }
+
+    private void userValidate(User user) throws BaseException {
+        if(user == null) {
+            throw new BaseException(ResponseStatus.USER_NOT_FOUND);
+        }
+    }
+
+    private Work workValidate(Long workId) throws BaseException {
+        return workRepository.findById(workId).orElseThrow(() -> new BaseException(ResponseStatus.WORK_NOT_FOUND));
+    }
+}
