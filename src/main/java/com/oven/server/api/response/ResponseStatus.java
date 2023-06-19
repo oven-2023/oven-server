a7e9ba048c940102260bf9e7e41785cee0637fc5
haen-su <haensuya@gmail.com>
2023-06-19 20:50:15 +0900

    feat: ott 추천 api 추가

--- src/main/java/com/oven/server/api/response/ResponseStatus.java
+++ src/main/java/com/oven/server/api/response/ResponseStatus.java
@@ -11,7 +11,9 @@ public enum ResponseStatus {
 
     FAIL_REGISTER(false, 400, "회원가입이 실패하였습니다."),
     WORK_NOT_FOUND(false, 404, "해당 작품이 존재하지 않습니다"),
-    USER_NOT_FOUND(false, 400, "유저를 찾을 수 없습니다.");
+    USER_NOT_FOUND(false, 400, "유저를 찾을 수 없습니다."),
+
+    NOT_ENOUGH_DATA(true, 200, "데이터가 충분하지 않습니다.");
 
 
     private final Boolean isSuccess;
