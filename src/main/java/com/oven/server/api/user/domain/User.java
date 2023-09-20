10931bdd699b0113bdf1414e30959ce349827906
haen-su <haensuya@gmail.com>
2023-09-20 20:43:41 +0900

    feat: User 도메인 생성

---
+++ src/main/java/com/oven/server/api/user/domain/User.java
@@ -0,0 +1,28 @@
+package com.oven.server.api.user.domain;
+
+import com.oven.server.common.BaseEntity;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class User extends BaseEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "user_id")
+    private Long id;
+
+    private String username;
+
+    private String nickname;
+
+    private String password;
+
+}
