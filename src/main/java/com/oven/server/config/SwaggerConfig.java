11bb3743bfceab20e06b732c1a5cf94aadfd7758
haen-su <haensuya@gmail.com>
2023-10-12 22:32:14 +0900

    fix: swagger 요청 url https 수정

--- src/main/java/com/oven/server/config/SwaggerConfig.java
+++ src/main/java/com/oven/server/config/SwaggerConfig.java
@@ -2,12 +2,15 @@ package com.oven.server.config;
 
 import io.swagger.v3.oas.annotations.OpenAPIDefinition;
 import io.swagger.v3.oas.annotations.info.Info;
+import io.swagger.v3.oas.annotations.servers.Server;
 import org.springframework.context.annotation.Configuration;
 
 @OpenAPIDefinition(
         info = @Info(title = "Oven 2023 API Document",
                 description = "2023 홍익대학교 컴퓨터공학과 졸업 프로젝트 Oven API 명세서",
-                version = "1.0.0"))
+                version = "1.0.0"),
+        servers = {@Server(url = "/")}
+        )
 @Configuration
 public class SwaggerConfig {
 }
