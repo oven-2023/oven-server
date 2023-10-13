25d6fc2414b54e09947588d747157b23babdabd1
haen-su <haensuya@gmail.com>
2023-10-13 22:16:53 +0900

    fix: spring security 설정 변경

--- src/main/java/com/oven/server/config/SecurityConfig.java
+++ src/main/java/com/oven/server/config/SecurityConfig.java
@@ -49,6 +49,8 @@ public class SecurityConfig {
                 .and()
                 .httpBasic().disable()
                 .csrf().disable()
+                .headers().frameOptions().disable()
+                .and()
                 .sessionManagement()
                 .sessionCreationPolicy(SessionCreationPolicy.STATELESS)
 
