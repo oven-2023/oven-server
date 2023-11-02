6f5bd092e315d2746b4009d845ba084941323bfd
haen-su <haensuya@gmail.com>
2023-11-02 14:57:59 +0900

    feat: add get my chatrooms api

--- src/main/java/com/oven/server/api/chat/repository/EntranceRepository.java
+++ src/main/java/com/oven/server/api/chat/repository/EntranceRepository.java
@@ -6,11 +6,14 @@ import com.oven.server.api.user.domain.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
 import java.util.Optional;
 
 @Repository
 public interface EntranceRepository extends JpaRepository<Entrance, Long> {
 
     Optional<Entrance> findByUserAndChatroom(User user, Chatroom chatroom);
+
+    List<Entrance> findByUser(User user);
 
 }
