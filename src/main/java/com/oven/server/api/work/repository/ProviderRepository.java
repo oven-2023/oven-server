dd937e01c8e0a73f648acfbfa5749a8cc91eca59
haen-su <haensuya@gmail.com>
2023-11-07 22:21:02 +0900

    feat: add crawling api

--- src/main/java/com/oven/server/api/work/repository/ProviderRepository.java
+++ src/main/java/com/oven/server/api/work/repository/ProviderRepository.java
@@ -4,7 +4,12 @@ import com.oven.server.api.work.domain.Provider;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+import java.util.Optional;
+
 @Repository
 public interface ProviderRepository extends JpaRepository<Provider, Long> {
+
+    Optional<Provider> findByName(String providerName);
 
 }
